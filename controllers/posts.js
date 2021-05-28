311921cac202a40d147e7331e68cbb851d0086e2
N3tw0rk <khoirrurrohman@gmail.com>
2021-05-28 13:18:53 +0700

    complete all route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -23,4 +25,29 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).json({ message: "not found" });
+
+  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatePost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: "not found" });
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ meesage: "deleted" });
 };
